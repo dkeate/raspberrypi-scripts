e2744d93700c46e8839b15a29c303737485e3beb
Dustin Keate <dkeate@gmail.com>
2026-08-23 11:11:54 +0900

    add conda update to update script

--- update-reboot.sh
+++ update-reboot.sh
@@ -4,6 +4,10 @@
 /home/pi/raspberrypi-scripts/docker-update.sh
 sleep 5
 
+# update conda base environment
+conda update --all
+sleep 5
+
 # update system
 sudo apt-get update
 sudo apt-get -y upgrade
